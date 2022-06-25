141c7cb042433e3d26f12d39934894931459f17a
JessicaJuliet <jessica.akomfrah@gmail.com>
2022-06-25 18:09:10 +0000

    Setup create react app project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+
+// Resource: Gitpod environemnt - https://github.com/gitpod-io/create-react-app
+// Tutorial: YouTube - https://www.youtube.com/watch?v=B1tjrnX160k
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div></div>
     </div>
   );
 }
