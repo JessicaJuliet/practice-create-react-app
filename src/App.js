b36a1ad8d2f83b0a4e8b8fb2ee3f6ee0d5a19fd1
JessicaJuliet <jessica.akomfrah@gmail.com>
2022-06-25 19:21:07 +0000

    Add circular progress bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
+import Timer from './Timer.js';
 
 // Resource: Gitpod environemnt - https://github.com/gitpod-io/create-react-app
 // Tutorial: YouTube - https://www.youtube.com/watch?v=B1tjrnX160k
 
 function App() {
   return (
-    <div className="App">
-      <div></div>
-    </div>
+    <main>
+      <Timer />
+    </main>
   );
 }
 
