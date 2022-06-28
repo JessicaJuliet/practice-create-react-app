123258b969797c74fa6019fd272c0fe18799aecb
JessicaJuliet <jessica.akomfrah@gmail.com>
2022-06-28 18:54:30 +0000

    Add settings & back button

--- src/App.js
+++ src/App.js
@@ -1,13 +1,30 @@
-import './App.css';
-import Timer from './Timer.js';
+import "./App.css";
+import Timer from "./Timer";
+import Settings from "./Settings";
+import {useState} from "react";
+import SettingsContext from "./SettingsContext";
 
 // Resource: Gitpod environemnt - https://github.com/gitpod-io/create-react-app
 // Tutorial: YouTube - https://www.youtube.com/watch?v=B1tjrnX160k
 
 function App() {
+
+  const [showSettings, setShowSettings] = useState(false);
+  const [workMinutes, setWorkMinutes] = useState(45);
+  const [breakMinutes, setBreakMinutes] = useState(15);
+
   return (
     <main>
-      <Timer />
+      <SettingsContext.Provider value={{
+        showSettings,
+        setShowSettings,
+        workMinutes,
+        breakMinutes,
+        setWorkMinutes,
+        setBreakMinutes,
+      }}>
+        {showSettings ? <Settings /> : <Timer />}
+      </SettingsContext.Provider>
     </main>
   );
 }
--- src/Timer.js
+++ src/Timer.js
@@ -2,11 +2,15 @@ import { CircularProgressbar, buildStyles } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 import PlayButton from './PlayButton';
 import PauseButton from './PauseButton';
+import SettingsButton from './SettingsButton';
+import {useContext} from "react";
+import SettingsContext from "./SettingsContext";
 
 const red = "#f54e4e"
-const green = "#4aecbc"
+// const green = "#4aecbc"
 
 function Timer() {
+    const settingsInfo = useContext(SettingsContext);
     return (
         <div>
             <CircularProgressbar value={60} text={'60%'} styles={buildStyles( 
@@ -20,6 +24,9 @@ function Timer() {
             <div style={{marginTop:'20px'}}> 
                 <PlayButton />
                 <PauseButton />
+            </div>
+            <div style={{marginTop:'20px'}}>
+                <SettingsButton onClick={() => settingsInfo.setShowSettings(true)} />
             </div>
         </div>
     );
